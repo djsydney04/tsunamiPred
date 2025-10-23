5304fe5e81450d0d55b7e09f7d86c932bf27368f
djsydney04 <dylanmitic@gmail.com>
2025-10-23 00:14:37 -0700

    semi working cli for testing

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -11,7 +11,7 @@ def preprocess():
     #reading features from dataframe (inputs)
     X = torch.tensor(df[['magnitude', 'cdi', 'mmi', 'sig', 'nst', 'dmin', 'gap', 'depth', 'latitude', 'longitude']].values, dtype=torch.float32)
     #reading target from dataframe (outputs)
-    y = torch.tensor(df['tsunami'].values, dtype = torch.float32).unsqueeze(1)
+    y = torch.tensor(df['tsunami'].values, dtype = torch.float32).unsqueeze(1)  
     #splitting data into training and testing sets
     x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.2,train_size=0.8,random_state=42)
     #returning training and testing sets
